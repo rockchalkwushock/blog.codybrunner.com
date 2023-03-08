7e9918721a0b3b7af89144f753533a89aa7e23ee
Cody Brunner <me@codybrunner.com>
2023-03-08 16:11:35 -0500

    chore(root): adds tailwind integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,16 @@
-import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	integrations: [
+		tailwind({
+			config: { applyBaseStyles: false },
+		}),
+	],
+	server: {
+		open: true,
+		port: 4000,
+	},
+	site: 'https://blog.codybrunner.com',
+})
