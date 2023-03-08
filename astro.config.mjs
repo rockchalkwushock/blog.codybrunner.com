cc3fdb555aebdcbe3ddcbfd149530b3c370132c2
Cody Brunner <me@codybrunner.com>
2023-03-08 18:09:55 -0500

    chore(root): updates astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,51 @@
-import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import tailwind from '@astrojs/tailwind'
+
+// Remark Plugins
+import remarkCodeTitle from 'remark-flexible-code-titles'
+import remarkExternalLinks from 'remark-external-links'
+import remarkNormalizeHeadings from 'remark-normalize-headings'
+import remarkSectionize from 'remark-sectionize'
+import remarkToc from 'remark-toc'
+import remarkValidateLinks from 'remark-validate-links'
+// Rehype Plugins
+import { rehypeHeadingIds } from '@astrojs/markdown-remark'
+import { rehypeAccessibleEmojis } from 'rehype-accessible-emojis'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import rehypeSlug from 'rehype-slug'
+// Custom Plugins
+import { remarkReadingTime } from './src/plugins/remark-reading-time.mjs'
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
+		mdx(),
 		tailwind({
 			config: { applyBaseStyles: false },
 		}),
 	],
+	markdown: {
+		rehypePlugins: [
+			rehypeHeadingIds,
+			rehypeSlug,
+			[rehypeAutolinkHeadings, { behavior: 'wrap' }],
+			rehypeAccessibleEmojis,
+		],
+		remarkPlugins: [
+			[remarkToc, { maxDepth: 4, ordered: false, tight: true }],
+			remarkNormalizeHeadings,
+			remarkSectionize,
+			remarkExternalLinks,
+			remarkReadingTime,
+			remarkCodeTitle,
+			remarkValidateLinks,
+		],
+		shikiConfig: {
+			theme: 'material-palenight',
+			wrap: true,
+		},
+	},
 	server: {
 		open: true,
 		port: 4000,
