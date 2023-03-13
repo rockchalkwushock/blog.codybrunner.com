ee9db8e158fe95ed425c757db4dcb2c9cf27c066
Cody Brunner <me@codybrunner.com>
2023-03-12 19:25:51 -0500

    chore(content): updates postSchema

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,12 +1,80 @@
+import type { CollectionEntry } from 'astro:content'
 import { defineCollection, z } from 'astro:content'
+
+const CATEGORIES = ['expat', 'personal', 'technology', 'travel'] as const
+const CategoriesEnum = z.enum(CATEGORIES)
+export type CategoryEnum = z.infer<typeof CategoriesEnum>
+
+const TAGS = [
+	'absinthe',
+	'asd',
+	'ash',
+	'astrojs',
+	'autism',
+	'aws',
+	'ci-cd',
+	'cli',
+	'cloudflare',
+	'colombia',
+	'deploy',
+	'django',
+	'dns',
+	'docker',
+	'docker-compose',
+	'ecto',
+	'elixir',
+	'erlang',
+	'family',
+	'fly',
+	'git',
+	'graphql',
+	'hasura',
+	'hugo',
+	'immigration',
+	'iterm',
+	'javascript',
+	'linux',
+	'macos',
+	'marriage',
+	'mdx',
+	'nextjs',
+	'netlify',
+	'nodejs',
+	'opinion',
+	'oss',
+	'phoenix',
+	'phoenix-liveview',
+	'pnpm',
+	'psql',
+	'python',
+	'raspberry-pi',
+	'reactjs',
+	'react-query',
+	'rust',
+	'solidjs',
+	'solidstart',
+	'sql',
+	'ssh',
+	'storybookjs',
+	'svelte',
+	'sveltekit',
+	'tailwindcss',
+	'typescript',
+	'vercel',
+	'vitejs',
+	'vitest',
+	'vscode',
+	'yarn',
+	'zsh',
+] as const
+const TagsEnum = z.enum(TAGS)
+export type TagEnum = z.infer<typeof TagsEnum>
 
 const postsSchema = z.object({
 	archived: z.boolean().default(false),
 	author: z.string().default('Cody Brunner'),
 	canonicalURL: z.string().url(),
-	categories: z
-		.array(z.enum(['expat', 'personal', 'technology', 'travel']))
-		.optional(),
+	categories: z.array(CategoriesEnum).optional(),
 	createdAt: z.string().transform(str => new Date(str)),
 	description: z.string(),
 	draft: z.boolean().default(true),
@@ -22,72 +90,7 @@ const postsSchema = z.object({
 		.string()
 		.transform(str => new Date(str))
 		.optional(),
-	tags: z
-		.array(
-			z.enum([
-				'absinthe',
-				'asd',
-				'ash',
-				'astrojs',
-				'autism',
-				'aws',
-				'ci-cd',
-				'cli',
-				'cloudflare',
-				'colombia',
-				'deploy',
-				'django',
-				'dns',
-				'docker',
-				'docker-compose',
-				'ecto',
-				'elixir',
-				'erlang',
-				'family',
-				'fly',
-				'git',
-				'graphql',
-				'hasura',
-				'hugo',
-				'immigration',
-				'iterm',
-				'javascript',
-				'linux',
-				'macos',
-				'marriage',
-				'mdx',
-				'nextjs',
-				'netlify',
-				'nodejs',
-				'opinion',
-				'oss',
-				'phoenix',
-				'phoenix-liveview',
-				'pnpm',
-				'psql',
-				'python',
-				'raspberry-pi',
-				'reactjs',
-				'react-query',
-				'rust',
-				'solidjs',
-				'solidstart',
-				'sql',
-				'ssh',
-				'storybookjs',
-				'svelte',
-				'sveltekit',
-				'tailwindcss',
-				'typescript',
-				'vercel',
-				'vitejs',
-				'vitest',
-				'vscode',
-				'yarn',
-				'zsh',
-			])
-		)
-		.optional(),
+	tags: z.array(TagsEnum).optional(),
 	title: z.string(),
 	updatedAt: z
 		.string()
@@ -98,6 +101,8 @@ const postsSchema = z.object({
 const postsCollection = defineCollection({ schema: postsSchema })
 
 export type PostSchema = z.infer<typeof postsSchema>
+
+export type Post = CollectionEntry<'posts'>
 
 export const collections = {
 	posts: postsCollection,
