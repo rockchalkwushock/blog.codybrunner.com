b034e625732da8104d1400b6ad14f47df5930ead
Cody Brunner <me@codybrunner.com>
2023-10-03 20:49:00 -0500

    feat(root): adds initial configs & layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,20 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import react from '@astrojs/react'
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	integrations: [react(), tailwind(), mdx()],
+	markdown: {
+		rehypePlugins: [],
+		remarkPlugins: [],
+		// Necessary to allow for `rehype-pretty-code` to work.
+		syntaxHighlight: false,
+	},
+	server: {
+		open: true,
+		port: 3001,
+	},
+	site: 'https://blog.codybrunner.com',
+})
