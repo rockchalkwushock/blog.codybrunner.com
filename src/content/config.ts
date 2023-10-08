656acffd03cdbed98fa38b5e1f0f1def573e00db
Cody Brunner <me@codybrunner.com>
2023-10-07 20:18:40 -0500

    refactor(content): refactors hierarchy

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,10 +1,107 @@
-import { articlesCollection, type Article } from './articles'
-import { authorsCollection, type Author } from './authors'
+import {
+	defineCollection,
+	reference,
+	z,
+	type CollectionEntry,
+} from 'astro:content'
+
+const authorSchema = z.object({
+	avatar: z.string().url(),
+	bio: z.string(),
+	email: z.string().email(),
+	github: z.string().url(),
+	instagram: z.string().url(),
+	linkedIn: z.string().url(),
+	name: z.string(),
+	telegram: z.string().url(),
+	twitter: z.string().url(),
+})
+
+const CATEGORIES = ['expat', 'personal', 'technology', 'travel'] as const
+const CategoriesEnum = z.enum(CATEGORIES)
+const categoriesSchema = z.array(CategoriesEnum).optional()
+
+const EXPAT_TAGS = [''] as const
+const PERSONAL_TAGS = [''] as const
+const TECH_TAGS = [
+	'astro',
+	'ci-cd',
+	'cli',
+	'cloudflare',
+	'deploy',
+	'ecto',
+	'elixir',
+	'github-actions',
+	'graphql',
+	'hugo',
+	'iterm',
+	'javascript',
+	'macos',
+	'nodejs',
+	'pnpm',
+	'psql',
+	'python',
+	'qwik',
+	'qwik-city',
+	'reactjs',
+	'ssh',
+	'tailwindcss',
+	'typescript',
+	'vscode',
+	'yarn',
+	'zsh',
+] as const
+const TRAVEL_TAGS = ['colombia'] as const
+const TAGS = [
+	...EXPAT_TAGS,
+	...PERSONAL_TAGS,
+	...TECH_TAGS,
+	...TRAVEL_TAGS,
+] as const
+const TagsEnum = z.enum(TAGS)
+const tagsSchema = z.array(TagsEnum).optional()
+
+const articleSchema = z.object({
+	archived: z.boolean().default(false),
+	author: reference('authors'),
+	canonicalURL: z.string().url(),
+	categories: categoriesSchema,
+	coverImage: z
+		.object({
+			alt: z.string(),
+			src: z.string().or(z.string().url()),
+		})
+		.optional(),
+	createdAt: z.string().transform(str => new Date(str)),
+	description: z.string(),
+	draft: z.boolean().default(true),
+	featured: z.boolean().default(false),
+	language: z.enum(['en', 'es']).default('en'),
+	publishedAt: z
+		.string()
+		.transform(str => new Date(str))
+		.optional(),
+	relatedArticles: z.array(reference('articles')).optional(),
+	tags: tagsSchema,
+	title: z.string(),
+	updatedAt: z
+		.string()
+		.transform(str => new Date(str))
+		.optional(),
+})
 
 export const collections = {
-	articles: articlesCollection,
-	authors: authorsCollection,
+	articles: defineCollection({
+		schema: articleSchema,
+		type: 'content',
+	}),
+	authors: defineCollection({
+		schema: authorSchema,
+		type: 'data',
+	}),
 }
 
-export type { Article, Author }
-export type { CategoryEnum, TagsEnum } from './taxonomy'
+export type Article = CollectionEntry<'articles'>
+export type Author = CollectionEntry<'authors'>
+export type CategoryEnum = z.infer<typeof CategoriesEnum>
+export type TagsEnum = z.infer<typeof TagsEnum>
