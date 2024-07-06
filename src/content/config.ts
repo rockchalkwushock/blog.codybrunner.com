4281c12fb669bd484b235ee49bf06476e65a984f
Cody Brunner <me@codybrunner.com>
2024-07-06 11:35:19 -0500

    feat(release): v1.12.0 (#35)
    * chore(root): removes items from UI
    
    * chore(content): updates employer information
    
    * chore(content): unarchives 2023 & 2022
    
    * chore(content): unarchives 2021
    
    * chore(content): unarchives 2019, 2018, & 2017
    
    * chore(content): removes unwritten post
    
    * chore(content): runs related-articles script
    
    * chore(release): version bump

--- src/content/config.ts
+++ src/content/config.ts
@@ -8,12 +8,10 @@ import {
 const authorSchema = z.object({
 	avatar: z.string().url(),
 	bio: z.string(),
-	coffee: z.string().url(),
 	email: z.string().email(),
 	github: z.string().url(),
 	linkedIn: z.string().url(),
 	name: z.string(),
-	twitter: z.string().url(),
 })
 
 const CATEGORIES = ['expat', 'personal', 'technology', 'travel'] as const
